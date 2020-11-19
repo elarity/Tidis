38dff08236d44690982df350de2e6186bd0e5c4b
elarity <42677833@qq.com>
2020-11-19 23:50:58 +0800

    ti_link

--- temp/tilink.c
+++ temp/tilink.c
@@ -62,12 +62,81 @@ void * add_tail(tilink_entry_struct * tilink_entry_point, tilink_node_struct * n
 }
 
 /*
+ * @desc : 根据index获取node
+ * */
+tilink_node_struct * get_node_by_index(tilink_entry_struct * tilink_entry_point, unsigned long index)
+{
+    unsigned long 
+    unsigned long length;
+    length = get_link_length(tilink_entry_point);
+    if (index > length) {
+        return NULL;
+    }
+
+}
+
+/*
  * @desc : 根据value获取node
  * */
 tilink_node_struct * get_node_by_value(tilink_entry_struct * tilink_entry_point, void * value)
 {
-    unsigned long length;
-    length = tilink_entry_point->length;
+    tilink_node_struct * node;
+    tilink_iterator_struct iterator;
+    reset_link_2_head(tilink_entry_point, &iterator);
+    // 开始遍历整个link，与value数值进行对比
+    while((node = link_next(&iterator)) != NULL) {
+        if (value == node->value) {
+            return node;
+        }
+    }
+    return NULL;
+}
 
+/*
+ * @desc : 一般用于while不断next
+ * */
+tilink_node_struct * link_next(tilink_iterator_struct * tilink_iterator_point)
+{
+    tilink_node_struct * current_node;
+    current_node = tilink_iterator_point->next;
+    if (current_node != NULL) {
+        if (ITERATOR_TAIL_TO_HEAD == tilink_iterator_point->direction)
+            tilink_iterator_point->next = current_node->next;
+        else
+            tilink_iterator_point->next = current_node->prev;
+    }
+    return current_node;
+}
 
+/*
+ * @desc : rewind link-list到head处
+ * */
+void reset_link_2_head(tilink_entry_struct * tilink_entry_point, tilink_iterator_struct * tilink_iterator_point)
+{
+    tilink_iterator_point->next      = tilink_entry_point->head;
+    tilink_iterator_point->direction = 0;
+}
+
+/*
+ * @desc : 返回头部node
+ * */
+tilink_node_struct * get_link_head(tilink_entry_struct * tilink_entry_point)
+{
+    return tilink_entry_point->head;
+}
+
+/*
+ * @desc : 返回尾巴node
+ * */
+tilink_node_struct * get_link_tail(tilink_entry_struct * tilink_entry_point)
+{
+    return tilink_entry_point->tail;
+}
+
+/*
+ * @desc : 获取link长度
+ * */
+unsigned long get_link_length(tilink_entry_struct * tilink_entry_point)
+{
+    return tilink_entry_point->length;
 }
--- temp/tilink.h
+++ temp/tilink.h
@@ -1,3 +1,5 @@
+#define ITERATOR_HEAD_TO_TAIL 0
+#define ITERATOR_TAIL_TO_HEAD 1
 // 链表entry
 typedef struct tilink_entry_struct {
     tilink_node_struct * head;  // 链表尾node
@@ -10,15 +12,32 @@ typedef struct tilink_node_struct {
     tilink_node_struct * next;
     void * value;  // 该node的value保存到此
 } tilink_node_struct;
+// 遍历用的临时节点结构
+typedef struct tilink_iterator_struct {
+    tilink_node_struct * next;
+    int direction;  // 表示方向 0表示从头到尾，1表示从尾到头
+} tilink_iterator_struct;
 
 // 创建一个空的list，不带任何node
 tilink_entry_struct * link_init();
 // 初始化一个node
 tilink_node_struct * node_init();
 // 添加一个head node
-void * add_head(tilink_entry_struct *, tilink_node_struct *);
+void add_head(tilink_entry_struct *, tilink_node_struct *);
 // 添加一个tail node
-void * add_tail(tilink_entry_struct *, tilink_node_struct *);
+void add_tail(tilink_entry_struct *, tilink_node_struct *);
 // 添加到指定位置node
+// 根据index获取node
+tilink_node_struct * get_node_by_index(tilink_entry_struct *, unsigned long);
 // 根据value获取node
 tilink_node_struct * get_node_by_value(tilink_entry_struct *, void *);
+// 遍历link的公用方法，双向
+tilink_node_struct * link_next(tilink_iterator_struct *);
+// rewind link-list到head处
+void reset_link_2_head(tilink_entry_struct *, tilink_iterator_struct *);
+// 返回头部node
+tilink_node_struct * get_link_head(tilink_entry_struct *);
+// 返回尾巴node
+tilink_node_struct * get_link_tail(tilink_entry_struct *);
+// 获取link长度
+unsigned long get_link_length(tilink_entry_struct *);
